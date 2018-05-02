91dfc2df1ef53bf9f37cffe01ce7e681da1bbc47
K.H Lee <beta1360@naver.com>
2018-05-03 00:44:02 +0900

    fix#2 update-module

--- Server/server.py
+++ Server/server.py
@@ -1,16 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, send_from_directory
 from util import util
-from exception import exception
+import exception
 
 util = util()
-exception = exception()
+ex = exception.exception()
 
-UPLOAD_FOLDER = 'upload'
+UPLOAD_FOLDER = 'uploaded'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/download", methods=["POST"])
+@app.route("/message", methods=["POST"])
 def send_image():
     data = request.get_json()
     user = data["user"]
@@ -20,7 +20,26 @@ def send_image():
         return jsonify(reply), 200
 
     except:
-        return jsonify(exception.error(user=user)), 500
+        return jsonify(ex.error(user=user,flag=exception.SERVER_ERROR)),500
+
+@app.route("/uploaded", methods=["POST"])
+def upload_image():
+    response = make_response()
+    try:
+        image = request.files['image']
+        image.save(image.filename)
+        response.status_code = 200
+    except Exception as e:
+        print(e)
+        response.status_code = 415
+    return response
+
+@app.route("/download/<filename>", methods=["GET"])
+def send_image_to_client(filename):
+    try:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename=filename)
+    except:
+        return jsonify(ex.error_nonfile(flag=exception.NON_EXISTENT_FILE)), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- Server/util.py
+++ Server/util.py
@@ -2,34 +2,44 @@ import time, requests
 
 class util:
     def __init__(self):
-        self.PATH = "upload/"
+        self.PATH = "uploaded/"
         self.HOST = "http://localhost:9000/image"
         self.HEADER = {"Content-Type": "application/json;charset=UTF-8"}
         self.SUCCESS = "이미지 수신에 성공하였습니다."
         self.FAIL = "이미지 수신에 실패하였습니다."
 
     def request_image(self):
-        response = requests.post(url=self.HOST, param=self.HEADER)
+        response = requests.post(url=self.HOST, headers=self.HEADER)
         return response
 
     def reply_analyzer(self):
         now = time.localtime()
         response = self.request_image()
-
         if response.status_code == 500:
             return False
         else:
-            return True
+            data = response.json()
+            filename = data["path"]
+            return True, filename
 
     def recieving_image_status(self, user):
-        if self.reply_analyzer() : sendMSG = self.SUCCESS
+        result, filename = self.reply_analyzer()
+        if result : sendMSG = self.SUCCESS
         else : sendMSG = self.FAIL
 
+        return self.make_message(user=user, sendMSG=sendMSG, filename=filename)
+
+    def make_message(self, user, sendMSG, filename):
         message = {
             "event": "send",
             "user" : user,
             "textContent": {
-                "text": sendMSG
+                "text": sendMSG + "\n- download link: "
+                        + self.make_link(filename=filename)
             }
         }
         return message
+
+    def make_link(self, filename):
+        link = "http://localhost:8000/download/"+self.PATH + filename
+        return link
